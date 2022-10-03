5eaa01096707a119e2220d2bd461d40113f3de5f
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-10-03 15:45:59 -0400

    Descripcion de los cofigos añadida
    This reverts commit 3a299f6a8e7a028592d5bfa4415189ebefa6d4b1.

--- Problema 2.5.c
+++ Problema 2.5.c
@@ -1,17 +1,16 @@
-#include <stdio.h> //Libreria
-#include <math.h> //Linreria
+#include <stdio.h>
+#include <math.h>
 /* Función matemática.
 El programa obtiene el resultado de una función.
 OP y T: variables de tipo entero.
 RES: variable de tipo real. */
 void main(void)
 {
-  int OP, T; //Define variables numericas con numeros de tipo entero
-  float RES; //Se utiliza para los numeros con puntos decimales
-  printf("Ingrese la opción del cálculo y el valor entero: "); //Presenta en la pantalla algun dato
-  scanf("%d %d", &OP, &T); //Lee caracteres ingresados desde el teclado
-  switch(OP) /*Permite selccionar el bloque de instucciones que se va a 
-  ejecutar dentro de varias opciones */
+  int OP, T;
+  float RES;
+  printf("Ingrese la opción del cálculo y el valor entero: ");
+  scanf("%d %d", &OP, &T);
+  switch(OP)
   {
     case 1: RES = T / 5;
       break;
