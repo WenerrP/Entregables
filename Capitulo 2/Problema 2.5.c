18038877e5df9029f20aaddcdb7d72d89d43815b
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-10-04 16:23:26 -0400

    Añadiendo nuevos archivos

---
+++ Capitulo 2/Problema 2.5.c
@@ -0,0 +1,29 @@
+#include <stdio.h> //Libreria
+#include <math.h> //Linreria
+/* Función matemática.
+El programa obtiene el resultado de una función.
+OP y T: variables de tipo entero.
+RES: variable de tipo real. */
+void main(void) //Es el punto de entrada al programa
+{
+  int OP, T; //Define variables numericas con numeros de tipo entero
+  float RES; //Se utiliza para los numeros con puntos decimales
+  printf("Ingrese la opcion del calculo y el valor entero: "); //Presenta en la pantalla algun dato
+  scanf("%d %d", &OP, &T); //Lee caracteres ingresados desde el teclado
+  switch(OP) /*Permite selccionar el bloque de instucciones que se va a 
+  ejecutar dentro de varias opciones */
+  {
+    case 1: RES = T / 5; /* Dependiendo del valor de la variable, 
+    ejecuta un bloque de instrucciones */
+      break; //Finaliza la ejecucion del bucle
+    case 2: RES = pow(T,T);
+    /* La función pow está definida en la biblioteca math.h */
+      break;
+    case 3:
+    case 4: RES = 6 * T/2;
+      break;
+    default: RES = 1;
+      break;
+  }
+  printf("\nResultado: %7.2f", RES); //Se imprime por pantalla el resultado de las operaciones
+} //Fin del programa
