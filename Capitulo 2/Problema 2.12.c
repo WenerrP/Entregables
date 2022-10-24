043f79f167c22050f494ebfe3b1eac18e1465b38
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-10-24 18:38:01 -0400

    Nuevos cambios

--- Capitulo 2/Problema 2.12.c
+++ Capitulo 2/Problema 2.12.c
@@ -7,19 +7,19 @@ una expresión determinada.
 T, P y N: variables de tipo entero. */
 int main(void) // Es el punto de entrada al programa
 {
-  int P, N; // Define variables numericas con numeros de tipo entero
-  float T;
-  printf("Ingrese los valores de T, P y N: "); // Presenta en la pantalla algun
-                                               // dato
-  scanf("%d %d %d", &T, &P, &N); // Lee caracteres ingresados desde el teclado
-  if (P != 0) /* Se utiliza para establece una condicion, si es verdadera
-  se ejecutaran las siguientes instrucciones */
-  {
-    if (pow(T / P, N) == (pow(T, N) / pow(P, N)))
-      printf("\nSe comprueba la igualdad");
-    else
-      printf("\nNo se comprueba la igualdad");
-  } else // Si la condicion es falsa pasa a esta linea de codigo
-    printf("\nP tiene que ser diferente de cero"); /* Luego presentando este
-    mensaje por pantalla */
+    int P, N; // Define variables numericas con numeros de tipo entero
+    float T;
+    printf("Ingrese los valores de T, P y N: "); // Presenta en la pantalla algun
+                                                // dato
+    scanf("%d %d %d", &T, &P, &N); // Lee caracteres ingresados desde el teclado
+    if (P != 0) /* Se utiliza para establece una condicion, si es verdadera
+    se ejecutaran las siguientes instrucciones */
+    {
+        if (pow(T / P, N) == (pow(T, N) / pow(P, N)))
+        printf("\nSe comprueba la igualdad");
+        else
+        printf("\nNo se comprueba la igualdad");
+    } else // Si la condicion es falsa pasa a esta linea de codigo
+        printf("\nP tiene que ser diferente de cero"); /* Luego presentando este
+        mensaje por pantalla */
 }
