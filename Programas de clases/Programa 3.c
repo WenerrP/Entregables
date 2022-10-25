66b2d981f52e0d8c15763f8fb64f8b5b1e6b5653
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-10-25 09:07:23 -0400

    Añadiendo archivos

--- Programas de clases/Programa 3.c
+++ Programas de clases/Programa 3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main() 
+{
     double x = 0.0;
     double b = 0.0;
     double tmp = 0.0;
@@ -10,27 +11,35 @@ int main() {
 
     printf("Calculo Raiz Cuadrada con Metodo de Newton\n");
 
-    while (c) {
+    while (c) 
+    {
         printf("\nDame el numero decimal: ");
         scanf("%lf", &x);
 
-        if (x <= 0) {
-        c = 0;
-        } else {
-        for (cont = 1, b = x; !(b == (x / b)); cont++) {
-            b = 0.5 * ((x / b) + b);
-            if (tmp != b) {
-            printf("\nCiclo %d valor actual es: %lf\n", cont, b);
-            tmp = b;
-            } else {
-            break;
-            }
-
-            if (cont > 100) 
+        if (x <= 0) 
+        {
+            c = 0;
+        } 
+        else 
+        {
+            for (cont = 1, b = x; !(b == (x / b)); cont++) 
             {
-            break;
+                b = 0.5 * ((x / b) + b);
+                if (tmp != b) 
+                {
+                    printf("\nCiclo %d valor actual es: %lf\n", cont, b);
+                    tmp = b;
+                } 
+                else 
+                {
+                    break;
+                }
+
+                if (cont > 100) 
+                {
+                    break;
+                }
             }
-        }
         }
     }
 
