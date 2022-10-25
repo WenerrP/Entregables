3b4270a17d61ab889820a24c57ee22529524941b
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-10-24 20:31:41 -0400

    Añadiendo archivos

--- Capitulo 3/Problema 3.8.c
+++ Capitulo 3/Problema 3.8.c
@@ -5,21 +5,21 @@ El programa calcula el factorial de un numero entero.
 
 FAC, I, NUM: variables de tipo entero. */
 
-void main (void) //Sirve como punto de entrada del programa
+int main(void) // Sirve como punto de entrada del programa
 {
-    int I, NUM; //Define variables numericas con numeros de tipo entero
-    long FAC; /* Es el tipo de dato de 80 bits de precisión extendida 
+  int I, NUM; // Define variables numericas con numeros de tipo entero
+  long FAC;   /* Es el tipo de dato de 80 bits de precisión extendida
     soportado por el hardware x86 */
-    printf("\nIngrese el numero: "); //Presenta en la pantalla algun dato
-    scanf("%d", NUM); //Lee caracteres ingresados desde el teclado
-    
-    if (NUM >= 0); //Se utiliza para establece una condicion
-    {
-        FAC = 1; //Inicializacion de la variable en 1
-        for(I = 1; I <= NUM; I++) //Lee caracteres ingresados desde el teclado
-            FAC += 1;
-        printf("\nEl factorial de %d es: %ld", NUM, FAC);
-    }
-    else //Si la condicion es falsa pasa a esta linea de codigo
-        printf("\nError en dato ingresado");
-} //Fin del programa
+  printf("\nIngrese el numero: "); // Presenta en la pantalla algun dato
+  scanf("%d", NUM);                // Lee caracteres ingresados desde el teclado
+
+  if (NUM >= 0)
+    ; // Se utiliza para establece una condicion
+  {
+    FAC = 1;                   // Inicializacion de la variable en 1
+    for (I = 1; I <= NUM; I++) // Lee caracteres ingresados desde el teclado
+      FAC += 1;
+    printf("\nEl factorial de %d es: %ld", NUM, FAC);
+  }
+  printf("\nError en dato ingresado");
+} // Fin del programa
