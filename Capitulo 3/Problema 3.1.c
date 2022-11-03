901a0dd3f6ca3fe222987d165e05e0b66a754c0c
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-11-02 23:15:38 -0400

    Nuevos archivos

--- Capitulo 3/Problema 3.1.c
+++ Capitulo 3/Problema 3.1.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-int main() //Sirve como punto de entrada del programa
+/* Nómina.
+El programa, al recibir los salarios de 15 profesores, obtiene el total de la
+nómina de la universidad.
+I: variable de tipo entero.
+SAL y NOM: variables de tipo real. */
+int main(void) 
 {
-    printf("Hello World\n"); //Presenta en la pantalla algun dato
-    system("Pause"); //programa espere a que el usuario pulse una tecla
-} //Fin del programa
+    int I;
+    float SAL, NOM;
+    NOM = 0;
+    for (I = 1; I <= 15; I++) 
+    {
+        printf("\nIngrese el salario del profesor %d: ", I);
+        scanf("%f", &SAL);
+        NOM += SAL;
+    }
+    printf("\nEl total de la nomina es : %.2f", NOM);
+}
