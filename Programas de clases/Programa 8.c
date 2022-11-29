155db8869349238f15f14c58be1f6cfc4b3f6493
Wenerr Valentín Pacheco Torres <202010282@itla.edu.do>
2022-11-28 21:43:02 -0400

    Archivos

--- Programas de clases/Programa 8.c
+++ Programas de clases/Programa 8.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /* Archivos y caracteres.
 El programa escribe caracteres en un archivo. */
+
 int main(void) 
 {
     char p1;
@@ -8,7 +9,7 @@ int main(void)
     ar = fopen("arc.txt", "w"); /* Se abre el archivo arc.txt para escritura. */
     if (ar != NULL) 
     {
-        while ((p1 = getchar()) != ‘\n’)
+        while ((p1 = getchar()) != '\n')
             /* Se escriben caracteres en el archivo mientras no se detecte el caracter
             que indica el fin de la línea. */
             fputc(p1, ar);
